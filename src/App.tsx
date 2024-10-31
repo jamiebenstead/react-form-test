592be35cf9b98432b3c0cb20d0ab3180dd51d8d0
jamiebenstead <jamie.benstead@gmail.com>
2024-10-31 15:04:47 +0000

    Change to use Carbon components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useState } from "react";
+import {
+  TextInput,
+  Button,
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+} from "@carbon/react";
 import "./custom.scss";
 
 type Person = {
@@ -62,54 +73,119 @@ function App() {
     URL.revokeObjectURL(url);
   };
 
+  const headers = [
+    { key: "firstName", header: "First Name" },
+    { key: "lastName", header: "Last Name" },
+    { key: "email", header: "Email" },
+    { key: "age", header: "Age" },
+  ];
+
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>First Name</label>
-          <input {...register("firstName")} />
-          <p>{errors.firstName?.message}</p>
+    <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ paddingBottom: "20px" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <TextInput
+            id="firstName"
+            labelText="First Name"
+            {...register("firstName")}
+            invalid={!!errors.firstName}
+            invalidText={errors.firstName?.message}
+          />
+          <TextInput
+            id="lastName"
+            labelText="Last Name"
+            {...register("lastName")}
+            invalid={!!errors.lastName}
+            invalidText={errors.lastName?.message}
+          />
+          <TextInput
+            id="email"
+            labelText="Email"
+            {...register("email")}
+            invalid={!!errors.email}
+            invalidText={errors.email?.message}
+          />
+          <TextInput
+            id="age"
+            labelText="Age"
+            type="number"
+            {...register("age")}
+          />
         </div>
-        <div>
-          <label>Last Name</label>
-          <input {...register("lastName")} />
-          <p>{errors.lastName?.message}</p>
-        </div>
-        <div>
-          <label>Email</label>
-          <input {...register("email")} />
-          <p>{errors.email?.message}</p>
-        </div>
-        <div>
-          <label>Age</label>
-          <input type="number" {...register("age")} />
-        </div>
-        <button type="submit">Add</button>
+        <Button
+          type="submit"
+          style={{
+            padding: "0 1rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Add to List
+        </Button>
       </form>
 
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {persons.map((person, index) => (
-            <tr key={index}>
-              <td>{person.firstName}</td>
-              <td>{person.lastName}</td>
-              <td>{person.email}</td>
-              <td>{person.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <button onClick={downloadCSV}>Download CSV</button>
-    </>
+      <div style={{ position: "relative" }}>
+        <Button
+          onClick={downloadCSV}
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            padding: "0 1rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Download CSV
+        </Button>
+        <div style={{ paddingTop: "48px" }}>
+          <DataTable
+            rows={persons.map((person, index) => ({
+              id: String(index),
+              firstName: person.firstName,
+              lastName: person.lastName,
+              email: person.email,
+              age: person.age,
+            }))}
+            headers={headers}
+          >
+            {({
+              rows,
+              headers,
+              getTableProps,
+              getHeaderProps,
+              getRowProps,
+            }) => (
+              <Table {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header) => {
+                      const { key, ...headerProps } = getHeaderProps({
+                        header,
+                      });
+                      return (
+                        <TableHeader key={key} {...headerProps}>
+                          {header.header}
+                        </TableHeader>
+                      );
+                    })}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow {...getRowProps({ row })}>
+                      {row.cells.map((cell) => (
+                        <TableCell key={cell.id}>{cell.value}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
+          </DataTable>
+        </div>
+      </div>
+    </div>
   );
 }
 
