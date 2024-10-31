5ff83ede4bc934fb29e6ccd75b785bc9db448640
jamiebenstead <jamie.benstead@gmail.com>
2024-10-31 12:22:48 +0000

    Install yup, add basic validation, add error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  age?: number;
+};
+
+const schema = yup
+  .object({
+    firstName: yup.string().required("First name is required"),
+    lastName: yup.string().required("Last name is required"),
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    age: yup.number().optional(),
+  })
+  .required();
 
 function App() {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data: Person) => {
     console.log("data", data);
   };
 
@@ -13,20 +39,23 @@ function App() {
         <div>
           <label>First Name</label>
           <input {...register("firstName")} />
+          <p>{errors.firstName?.message}</p>
         </div>
         <div>
           <label>Last Name</label>
           <input {...register("lastName")} />
+          <p>{errors.lastName?.message}</p>
         </div>
         <div>
           <label>Email</label>
-          <input type="email" {...register("email")} />
+          <input {...register("email")} />
+          <p>{errors.email?.message}</p>
         </div>
         <div>
           <label>Age</label>
           <input type="number" {...register("age")} />
         </div>
-        <button type="submit">Log out values</button>
+        <button type="submit">Add</button>
       </form>
     </>
   );
