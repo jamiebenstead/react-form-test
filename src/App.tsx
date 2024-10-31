ebe2ccbe7c4ff3db8f2b80df1bf813d85d349e90
jamiebenstead <jamie.benstead@gmail.com>
2024-10-31 12:14:08 +0000

    Install react-hook-form, create basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
-  return <></>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log("data", data);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>First Name</label>
+          <input {...register("firstName")} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input {...register("lastName")} />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" {...register("email")} />
+        </div>
+        <div>
+          <label>Age</label>
+          <input type="number" {...register("age")} />
+        </div>
+        <button type="submit">Log out values</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
